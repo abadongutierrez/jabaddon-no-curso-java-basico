1d80ea9b8eed5930cb50fe3ed266d41b45fda80a
abadongutierrez <abadon.gutierrez@gmail.com>
2024-05-14 19:58:38 -0600

    Switch

--- controlflow/SwitchExpressionYieldExample.java
+++ controlflow/SwitchExpressionYieldExample.java
@@ -1,6 +1,28 @@
 
 void main() {
     System.out.println(usingYield("Foo"));
+    otroMetodo("Foo");
+    otroMetodo("Bar");
+    otroMetodo("Baz");
+}
+
+int otroMetodo(String str) {
+    switch (str) {
+        case "Foo" -> {
+            System.out.println("Foo was matched!"); 
+            return 1;
+        }
+        case "Bar" -> {
+            System.out.println("Bar was matched!");
+            return 2;
+        }
+        default -> {
+            System.out.println("Default case");
+            return 0;
+        }
+    }
+    // Unreachable code
+    //System.out.println("After switch");
 }
 
 int usingYield(String s) {
